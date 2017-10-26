47c828b528b71cb1b400b22b63acd7454dfacc07
John Israel Aquino <30685835+johnisrael@users.noreply.github.com>
2017-10-26 19:14:45 +0800

    header.php cleaned

--- header.php
+++ header.php
@@ -1,14 +1,13 @@
 <?php
+	// HEADER
 
-// HEADER
+	echo 
+	'<div id="heading" style="background: linear-gradient(to bottom right, rgba(92, 184, 92, 0.9) 20%, rgba(92, 184, 92, 0) 100%); padding: 10px; width: calc(100% - 100px); height:60px; margin-bottom: 10px;">
+		<div>
+			<img class="media-object" style="float:left; height:40px; margin-left:15px; opacity:0.9;" src="/images/luke_logo.bmp">
+		</div>
+		<p style="float:left; font-size:16pt; color:#ffffff; margin: 5px 20px;"> LUKE FOUNDATION, INC. </p>
+	</div>';
 
-echo '<div id="heading" style="background: linear-gradient(to bottom right, rgba(92, 184, 92, 0.9) 20%, rgba(92, 184, 92, 0) 100%); padding: 10px; width: calc(100% - 100px); height:60px; margin-bottom: 10px;">
-  <div>
-  <img class="media-object" style="float:left; height:40px; margin-left:15px; opacity:0.9;" src="/images/luke_logo.bmp">
-  </div>
-  <p style="float:left; font-size:16pt; color:#ffffff; margin: 5px 20px;"> LUKE FOUNDATION, INC. </p>
-</div>';
-
-// HEADER END
-
+	// HEADER END
 ?>
