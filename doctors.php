ff7f218a2b3a56c6241cd40682c35fdf6de3dd0e
Karl Mabutas <31471891+karlbenson@users.noreply.github.com>
2017-10-26 20:10:17 +0800

    [MINOR] Changed website titles
    so you don't have to =)

--- doctors.php
+++ doctors.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Prototype</title>
+		<title>Luke Foundation Eye Program: Doctors</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="references/bootstrap.min.css">
