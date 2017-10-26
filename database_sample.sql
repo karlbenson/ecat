0e83b7f44a236b2d2c018810fef0037ff323d808
John Israel Aquino <30685835+johnisrael@users.noreply.github.com>
2017-10-26 18:17:21 +0800

    SQL code cleaned
    ez

--- database_sample.sql
+++ database_sample.sql
@@ -4,55 +4,30 @@ drop database lukedb;
 create database lukedb;
 use lukedb;
 
-
 create table DOCTOR(
-
 	DOC_LICENSE_NUM  CHAR(7) NOT NULL UNIQUE,
-
 	LAST_NAME VARCHAR(20) NOT NULL,
-
 	FIRST_NAME VARCHAR(20)  NOT NULL,
 	ADDRESS VARCHAR(50),
-
 	SPECIALIZATION VARCHAR(25),
 	PRIMARY KEY (DOC_LICENSE_NUM)
 );
 
-
-/*SAMPLE DATA*/
-
 INSERT INTO DOCTOR VALUES
-
 	('0000001','Smith','John','West Virginia. 26 Santos Street.','Anesthesiologist'),
-
 	('0000002','South','Johan','West Virginia. 15 Santos Street.','Internist'),
-
 	('0000003','Reefs','James','West Virginia. 106 Santos Street.','Anesthesiologist'),
-
 	('0000004','Reefs','Jessie','West Virginia. 106 Santos Street.','Surgeon'),
-
 	('0000005','Nedd','Milah','Florida, Border Street Avenue, Block 56.','Surgeon'),
-
 	('0000006','Colonel','Sanders','Kentucky, Fried Chicken.','Internist'),
-
 	('0000007','Ricardo','Heart','09 Silver Road, Beckham Park.','Anesthesiologist'),
-
 	('0000008','Ketchup','Ash','Pallet Town, West Kanto Region.','Internist'),
-
 	('0000009','Viva','Giovanni','Soul Silver Street, Viridian City, West Kanto Region.','Surgeon'),
-
 	('0000010','Crystal','Grey','City of Evergreen, Central Johto Region.','Internist'),
-
 	('0000011','Light','Lilian','','Surgeon'),
-
 	('0000012','Violet','Vivi','Route 26, Cinnabar Island, Kanto Region.','Internist'),
-
 	('0000013','Lilam','Loren','Route 205, Floaroma Town, Sinnoh Region.','Anesthesiologist')
 ;
-
-
-/*SAMPLE DATA END*/
-
 
 create table EYEPATIENT(
 	PAT_ID_NUM VARCHAR(15) NOT NULL UNIQUE,
@@ -61,55 +36,40 @@ create table EYEPATIENT(
 	PAT_AGE VARCHAR(6) NOT NULL,
 	PAT_PH CHAR(1) NOT NULL,
 	PAT_SEX CHAR(1) NOT NULL,
-
 	PHY_LICENSE_NUM CHAR(7) NOT NULL,
-
 	STAFF_LICENSE_NUM CHAR(7) NOT NULL,
-
 	PRE_VA_WITH_SPECT_LEFT VARCHAR(7) NOT NULL,
 	POST_VA_WITH_SPECT_LEFT VARCHAR(7) NOT NULL,
 	PRE_VA_WITH_SPECT_RIGHT VARCHAR(7) NOT NULL,
 	POST_VA_WITH_SPECT_RIGHT VARCHAR(7) NOT NULL,
-	
 	PRE_VA_NO_SPECT_LEFT VARCHAR(7) NOT NULL,
 	POST_VA_NO_SPECT_LEFT VARCHAR(7) NOT NULL,
 	PRE_VA_NO_SPECT_RIGHT VARCHAR(7) NOT NULL,
 	POST_VA_NO_SPECT_RIGHT VARCHAR(7) NOT NULL,
-
 	VISUAL_DISABILITY VARCHAR(15),
 	DISABILITY_CAUSE VARCHAR(30),
 	DIAGNOSIS VARCHAR(15),
 	PROCEDURE_TO_DO VARCHAR(15),
-	
 	RIGHT_EYE_AFFECTED VARCHAR(12),
 	LEFT_EYE_AFFECTED VARCHAR(12),
-	
 	PRIMARY KEY (PAT_ID_NUM),
 	FOREIGN KEY (PHY_LICENSE_NUM) references DOCTOR(DOC_LICENSE_NUM) on update cascade
 );
-/*SAMPLE DATA */
+
 INSERT INTO EYEPATIENT VALUES
 	('00000011','Johnny','Sins', '34', 'Y', 'M','0000001','0000002','20/32','20/20','20/12.5','20/12.5','20/32','20/20','20/12.5','20/12.5','excess eye', 'too much dota2', 'sample diag', 'remove eye','unknown','unknown');
-create table SURGERY(
 
+CREATE TABLE SURGERY(
 	CASE_NUM CHAR(10) NOT NULL UNIQUE,
-
 	SURG_LICENSE_NUM CHAR(7) NOT NULL,
-
 	PAT_ID_NUM VARCHAR(15) NOT NULL,
-
 	VISUAL_IMPARITY VARCHAR(100) NOT NULL,
-
 	MED_HISTORY VARCHAR(100),
-
 	DIAGNOSIS VARCHAR(100),
-
 	SURG_ANESTHESIA vARCHAR(25),
 	SURG_ADDRESS VARCHAR(50),
-
 	SURG_DATE DATE NOT NULL,
 	REMARKS VARCHAR(100),
-
 	INTERNIST VARCHAR(40),
 	ANESTHESIOLOGIST VARCHAR(40),
 	IOLPOWER VARCHAR(20),
@@ -121,8 +81,6 @@ create table SURGERY(
 	CSF_SUPPLIES DECIMAL(8,2),
 	CSF_LAB DECIMAL(8,2),
 	PRIMARY KEY (CASE_NUM),
-
 	FOREIGN KEY (SURG_LICENSE_NUM) REFERENCES DOCTOR(DOC_LICENSE_NUM) on update cascade,
-
 	FOREIGN KEY (PAT_ID_NUM) REFERENCES EYEPATIENT(PAT_ID_NUM) on update cascade
 );
