939f01ad52c5e151ea8b002a9a4d028d76f874f2
John Israel Aquino <30685835+johnisrael@users.noreply.github.com>
2017-10-30 01:44:20 +0800

    dbconnect.php cleaned

--- dbconnect.php
+++ dbconnect.php
@@ -1,13 +1,13 @@
 <?php
-$server = "localhost";  //own database server name
-$username = "root"; //own database username
-$password = "root";  //own database password
-$database = "lukedb";  //own database name
+	$server = "localhost";  //own database server name
+	$username = "root"; //own database username
+	$password = "root";  //own database password
+	$database = "lukedb";  //own database name
 
-  //CONNECT TO SERVER
-$mydatabase = new mysqli($server, $username, $password, $database);
-  //IF CONNECTION FAILED
-if (!$mydatabase) {
-  die( '<div style="color: #ffffff; font-size: 12pt; text-align:center;">'.'Error: Unable to connect to database.'.'</div');
-}//END
+	//CONNECT TO SERVER
+	$mydatabase = new mysqli($server, $username, $password, $database);
+	//IF CONNECTION FAILED
+	if (!$mydatabase) {
+		die( '<div style="color: #ffffff; font-size: 12pt; text-align:center;">'.'Error: Unable to connect to database.'.'</div');
+	}//END
 ?>
