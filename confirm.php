3093eec92ad6598472bd25791a7774fdd4327d6e
rroryan <31471067+rroryan@users.noreply.github.com>
2017-11-04 19:10:53 +0800

    [modified to delete at external]

--- confirm.php
+++ confirm.php
@@ -9,11 +9,19 @@
 						<button type="button" class="close" data-dismiss="modal">&times;</button>
 						<h4 class="modal-title">Confirmation</h4>
 					</div>
-					<div class="modal-body">
-						<p>Are you sure you want to delete this record?</p>
-						<div style="display:none;" id="id_ident" >'.$_GET["profilepage"].'</div>
-					</div>
-					<div class="modal-footer">
+					<div class="modal-body">';
+
+							echo '<p>Are you sure you want to delete this record?</p>';
+
+							if(isset($_GET["profilepage"])){
+								$to_del = $_GET["profilepage"];
+								echo '<p style="text-align:center; font-weight:bold;">'.'  Identification No. : '.'<span id="id_ident">'.$to_del.'</span></p>';
+							}else{
+								echo '<p style="text-align:center; font-weight:bold;">'.'  Identification No. : '.'<span id="id_ident"></span></p>';
+							}
+
+					echo '</div>
+					<div class="modal-footer" style="text-align:center;">
 						<button type="button" class="btn btn-default" onclick="redirect_this()" >Delete</button>
 						<button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
 					</div>
@@ -26,9 +34,16 @@
 	echo '<script>
 			function redirect_this(){
 				var profile_p = document.getElementById("id_ident").innerHTML;
+
 				var delete_token = document.getElementById("del_button").value;
 				var link = delete_token+".php?delete="+profile_p;
 				window.location = link;
+			}
+
+			function outer_close(e){
+				var bring_token = e;
+				$("#confirm_this").modal("show");
+				document.getElementById("id_ident").innerHTML = e;
 			}
 		</script>';
 		//SCRIPT END
