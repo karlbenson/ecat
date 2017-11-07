c4ecc3013e1cf86d098127af0b95b67dbe3a7a71
Patricio Casurao <ptcasurao@gmail.com>
2017-11-07 23:31:15 +0800

    [MINOR] input field size

--- form_patient.php
+++ form_patient.php
@@ -291,7 +291,7 @@
 														<div class="form-group row">
 															<label class="control-label col-md-1" for="P_LEA" style="float:left; width:40%; float:left;">Left Eye</label>
 															<div class="col-md-4" style="width: 60%; float:left;">
-															<div style="max-width:200px;">
+															<div style="max-width:300px;">
 																<input type="" class="form-control" id="P_LEA" placeholder="Affected Area of Eye" maxlength="<?php echo $LEA_MAX; ?>" name="P_LEA">
 															</div>
 															</div>
@@ -347,27 +347,16 @@
 
 <?php
 	include("dbconnect.php");
- $surgeon = $mydatabase->query("SELECT FIRST_NAME,LAST_NAME,DOC_LICENSE_NUM from DOCTOR");
+ $phy = $mydatabase->query("SELECT FIRST_NAME,LAST_NAME,DOC_LICENSE_NUM from DOCTOR");
  $arr = array();
  
- while ($row = $surgeon->fetch_assoc()) {
+ while ($row = $phy->fetch_assoc()) {
   unset($id, $name1, $name2);
   $id = $row['FIRST_NAME']." ".$row['LAST_NAME']."-".$row['DOC_LICENSE_NUM'];
   //$name1 = $row['FIRST_NAME'];
   //$name2 = $row['LAST_NAME'];
   array_push($arr, $id/*.", ".$name2." ".$name1*/);
  }
-				
-	$patient = $mydatabase->query("SELECT PAT_FNAME,PAT_LNAME,PAT_ID_NUM from EYEPATIENT");
-    $arr1 = array();
-    
- while ($row = $patient->fetch_assoc()) {
-		unset($id, $name1, $name2);
-  $id = $row['PAT_FNAME']." ".$row['PAT_LNAME']."-".$row['PAT_ID_NUM'];
-  //$name1 = $row['FIRST_NAME'];
-		//$name2 = $row['LAST_NAME'];
-  array_push($arr1, $id/*.", ".$name2." ".$name1*/);    
-	}
 ?>
 
 <script type="text/javascript">
