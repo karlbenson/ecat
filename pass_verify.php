50f11de353c906376be78c2b9279778951d65d09
rroryan <31471067+rroryan@users.noreply.github.com>
2017-11-07 17:49:11 +0800

    [edited]

--- pass_verify.php
+++ pass_verify.php
@@ -2,7 +2,7 @@
 
 //USING TEMPORARY PASSWORD "ABC"
 
-echo '<div class="modal fade" id="enter_pass" role="dialog" style="margin-top:100px;">
+echo '<div style="margin-top:200px;" class="modal fade" id="enter_pass" role="dialog">
 	<div class="modal-dialog modal-sm">';
     
 // VERIFY CONTENT
@@ -19,9 +19,6 @@ echo '<div class="modal-content">
 	    		<div style="margin:0px;"> Wrong Password.</div>
 	  		</div>
   		</div>
-    	<!--<div class="container-fluid" style="width:100%;">
-      	<p>Surgery information is sensitive, therefore authorization is needed.</p>
-      </div>-->
       <div class="container-fluid" style="width:100%; text-align:center;">
     		<input type="password" class="form-control" id="verity" style="width:200px; margin:auto;" name="authorization" maxlength="36">
   		</div>
@@ -31,7 +28,7 @@ echo '<div class="modal-content">
 
   <div class="modal-footer" style="text-align:center;">
     <button type="button" class="btn btn-default" onclick="redirect_pass()" id="verify">Verify</button>
-    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+    <button type="button" class="btn btn-default" onclick="cancel_down()" >Cancel</button>
   </div>
 </div>';
 // CONFIRM CONTENT END
@@ -51,13 +48,18 @@ echo '<script>
   	var password_input = document.getElementById("verity").value;
   	var password = "ABC";
   	if(password==password_input){
-  		document.getElementById("verity").value = "...";
+  		document.getElementById("verity").value = "";
   		$("#enter_pass").modal("hide");
   		go_on();
   	}else{
   		$("#wrong_pass").collapse("show");
   		document.getElementById("verity").value = "";
   	}
+  }
+  function cancel_down(){
+    document.getElementById("verity").value = "";
+    $("#wrong_pass").collapse("hide");
+    $("#enter_pass").modal("hide");
   }
 
 </script>';
