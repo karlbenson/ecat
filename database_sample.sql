7ef289b80ba869e9b4a894bd94a561fd7f5f4879
John Israel Aquino <30685835+johnisrael@users.noreply.github.com>
2017-11-12 04:06:27 +0800

    Anyare sa file nato... ito dapat ung ready to copy/paste nalang nalang kung magpapalit ng db eh. kulang kulang naman na ung laman

--- database_sample.sql
+++ database_sample.sql
@@ -1,5 +1,18 @@
 
 
+drop database lukedb
+create database lukedb
+use lukedb
+
+
+create table patient(
+patient_id int unsigned not null auto_increment primary key,
+patient_fname varchar(30) not null,
+patient_lname varchar(30) not null,
+patient_minitial char(1) not null
+);
+
+
 create table DOCTOR(
 	DOC_LICENSE_NUM  CHAR(7) NOT NULL UNIQUE,
 	LAST_NAME VARCHAR(20) NOT NULL,
