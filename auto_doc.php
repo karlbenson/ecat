90e3742b562bd1025b0f544f837e42b8d0ec91db
rroryan <31471067+rroryan@users.noreply.github.com>
2017-11-12 23:13:32 +0800

    (for editing in patient and surgery)

--- auto_doc.php
+++ auto_doc.php
@@ -1,13 +1,12 @@
 
 <?php
-	include("dbconnect.php");
     $surgeon = $mydatabase->query("SELECT FIRST_NAME,LAST_NAME,DOC_LICENSE_NUM from DOCTOR");
     $arr = array();
     
     while ($row = $surgeon->fetch_assoc()) {
         unset($id, $name1, $name2);
-        $id = $row['DOC_LICENSE_NUM']." - ".$row['FIRST_NAME']." ".$row['LAST_NAME'];
-        array_push($arr, $id/*.", ".$name2." ".$name1*/);
+        $id = $row['FIRST_NAME']." ".$row['LAST_NAME']." - ".$row['DOC_LICENSE_NUM'];
+        array_push($arr, $id);
     }
 				
 	$patient = $mydatabase->query("SELECT PAT_FNAME,PAT_LNAME,PAT_ID_NUM from EYEPATIENT");
@@ -16,45 +15,66 @@
     while ($row = $patient->fetch_assoc()) {
 		unset($id, $name1, $name2);
         $id = $row['PAT_FNAME']." ".$row['PAT_LNAME']." - ".$row['PAT_ID_NUM'];
-        array_push($arr1, $id/*.", ".$name2." ".$name1*/);    
+        array_push($arr1, $id);    
 	}
-
 
 	$staff = $mydatabase->query("SELECT * from STAFF");
     $arr2 = array();
     
     while ($row = $staff->fetch_assoc()) {
-        $id = $row["STAFF_LICENSE_NUM"]." - ".$row['STAFF_FNAME']." ".$row['STAFF_LNAME'];
+        $id = $row['STAFF_FNAME']." ".$row['STAFF_LNAME']." - ".$row["STAFF_LICENSE_NUM"];
         array_push($arr2, $id);    
 	}
 
+
 echo '<script type="text/javascript">
 	$(document).ready(function(){
-		var arrs0 = '.json_encode($arr).';
-		var arrs1 = '.json_encode($arr1).';
-		var arrs2 = '.json_encode($arr2).';
-		var arrs0 = new Bloodhound({
+		var arrs='.json_encode($arr).';
+		var arrs = new Bloodhound({
 			datumTokenizer: Bloodhound.tokenizers.whitespace,
 			queryTokenizer: Bloodhound.tokenizers.whitespace,
-			local: arrs0
-		});
-		var arrs2 = new Bloodhound({
-			datumTokenizer: Bloodhound.tokenizers.whitespace,
-			queryTokenizer: Bloodhound.tokenizers.whitespace,
-			local: arrs2
+			local: arrs
 		});
 		
-		$("#P_PHYLIC").typeahead({
+		$(".typeahead").typeahead({
 			hint: true, highlight: true, minLength: 1 },
 		{
-			name: "arrs0", source: arrs0
-		});
-		$("#P_STAFFLIC").typeahead({
-			hint: true, highlight: true, minLength: 1 },
-		{
-			name: "arrs2", source: arrs2
+			name: "arrs", source: arrs
 		});
 	});
+
+	$(document).ready(function(){
+		var arrs= '.json_encode($arr1).';
+		var arrs = new Bloodhound({
+			datumTokenizer: Bloodhound.tokenizers.whitespace,
+			queryTokenizer: Bloodhound.tokenizers.whitespace,
+			local: arrs
+		});
+		
+		// Initializing the typeahead
+		$(".typeahead1").typeahead({
+			hint: true, highlight: true, minLength: 1 },
+		{
+			name: "arrs", source: arrs
+		});
+	});
+
+	$(document).ready(function(){
+		var arrs= '.json_encode($arr2).';
+		var arrs = new Bloodhound({
+			datumTokenizer: Bloodhound.tokenizers.whitespace,
+			queryTokenizer: Bloodhound.tokenizers.whitespace,
+			local: arrs
+		});
+		
+		// Initializing the typeahead
+		$(".typeahead2").typeahead({
+			hint: true, highlight: true, minLength: 1 },
+		{
+			name: "arrs", source: arrs
+		});
+	});
+
 </script>';
 
 ?>
